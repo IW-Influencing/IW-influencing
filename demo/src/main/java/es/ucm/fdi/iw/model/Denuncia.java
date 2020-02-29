cd66470e7347db893535c4af00c6169e71d82db5
loptor5 <calopezd@ucm.es>
2020-02-29 22:52:37 +0100

    Añadido modelo getters y setters Denuncia y Perfil_RRSS

--- demo/src/main/java/es/ucm/fdi/iw/model/Denuncia.java
+++ demo/src/main/java/es/ucm/fdi/iw/model/Denuncia.java
@@ -1,5 +1,110 @@
 package es.ucm.fdi.iw.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
 public class Denuncia {
 
+    private long idDenuncia;
+	private Usuario denunciante;
+	private Usuario denunciado;
+	private String denuncia;
+	private boolean tramitada;
+
+    public Denuncia() {
+    }
+
+    public Denuncia(long idDenuncia, Usuario denunciante, Usuario denunciado, String denuncia, boolean tramitada) {
+        this.idDenuncia = idDenuncia;
+        this.denunciante = denunciante;
+        this.denunciado = denunciado;
+        this.denuncia = denuncia;
+        this.tramitada = tramitada;
+    }
+
+    @Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+    public long getIdDenuncia() {
+        return this.idDenuncia;
+    }
+
+    public void setIdDenuncia(long idDenuncia) {
+        this.idDenuncia = idDenuncia;
+    }
+
+    @ManyToOne(targetEntity=Usuario.class)
+    public Usuario getDenunciante() {
+        return this.denunciante;
+    }
+
+    public void setDenunciante(Usuario denunciante) {
+        this.denunciante = denunciante;
+    }
+
+    @ManyToOne(targetEntity=Usuario.class)
+    public Usuario getDenunciado() {
+        return this.denunciado;
+    }
+
+    public void setDenunciado(Usuario denunciado) {
+        this.denunciado = denunciado;
+    }
+
+    public String getDenuncia() {
+        return this.denuncia;
+    }
+
+    public void setDenuncia(String denuncia) {
+        this.denuncia = denuncia;
+    }
+
+    public boolean isTramitada() {
+        return this.tramitada;
+    }
+
+    public boolean getTramitada() {
+        return this.tramitada;
+    }
+
+    public void setTramitada(boolean tramitada) {
+        this.tramitada = tramitada;
+    }
+
+    public Denuncia idDenuncia(long idDenuncia) {
+        this.idDenuncia = idDenuncia;
+        return this;
+    }
+
+    public Denuncia denunciante(Usuario denunciante) {
+        this.denunciante = denunciante;
+        return this;
+    }
+
+    public Denuncia denunciado(Usuario denunciado) {
+        this.denunciado = denunciado;
+        return this;
+    }
+
+    public Denuncia denuncia(String denuncia) {
+        this.denuncia = denuncia;
+        return this;
+    }
+
+    public Denuncia tramitada(boolean tramitada) {
+        this.tramitada = tramitada;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+            " idDenuncia='" + getIdDenuncia() + "'" +
+            ", denunciante='" + getDenunciante() + "'" +
+            ", denunciado='" + getDenunciado() + "'" +
+            ", denuncia='" + getDenuncia() + "'" +
+            ", tramitada='" + isTramitada() + "'" +
+            "}";
+    }
 }
--- demo/src/main/java/es/ucm/fdi/iw/model/Perfil_RRSS.java
+++ demo/src/main/java/es/ucm/fdi/iw/model/Perfil_RRSS.java
@@ -1,5 +1,84 @@
 package es.ucm.fdi.iw.model;
 
+import java.io.File;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
 public class Perfil_RRSS {
+
+    private long idPerfil;
+	private Usuario influencer;
+	private String nombre;
+	private String[] numSeguidores;
+	private File imagen;
+
+	public Perfil_RRSS() {
+	}
+
+	public Perfil_RRSS(long idPerfil, Usuario influencer, String nombre, String[] numSeguidores, File imagen) {
+		this.idPerfil = idPerfil;
+		this.influencer = influencer;
+		this.nombre = nombre;
+		this.numSeguidores = numSeguidores;
+		this.imagen = imagen;
+		
+	}
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	public long getIdPerfil() {
+		return this.idPerfil;
+	}
+
+	public void setIdPerfil(long idPerfil) {
+		this.idPerfil = idPerfil;
+	}
+
+	@ManyToOne(targetEntity=Usuario.class)
+	public Usuario getInfluencer() {
+		return this.influencer;
+	}
+
+	public void setInfluencer(Usuario influencer) {
+		this.influencer = influencer;
+	}
+
+	public String getNombre() {
+		return this.nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String[] getNumSeguidores() {
+		return this.numSeguidores;
+	}
+
+	public void setNumSeguidores(String[] numSeguidores) {
+		this.numSeguidores = numSeguidores;
+	}
+
+	public File getImagen() {
+		return this.imagen;
+	}
+
+	public void setImagen(File imagen) {
+		this.imagen = imagen;
+	}
+
+	@Override
+	public String toString() {
+		return "{" +
+			" idPerfil='" + getIdPerfil() + "'" +
+			", influencer='" + getInfluencer() + "'" +
+			", nombre='" + getNombre() + "'" +
+			", numSeguidores='" + getNumSeguidores() + "'" +
+			", imagen='" + getImagen() + "'" +
+			"}";
+	}
 
 }
