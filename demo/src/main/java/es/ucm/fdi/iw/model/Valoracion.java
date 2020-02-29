4d9891073bd8ef84c2041f9cc7dc4394a5f2c1c5
PabloMorientes <pmorientesl@gmail.com>
2020-02-29 21:02:11 +0100

    Add files via upload

--- demo/src/main/java/es/ucm/fdi/iw/model/Valoracion.java
+++ demo/src/main/java/es/ucm/fdi/iw/model/Valoracion.java
@@ -1,5 +1,57 @@
 package es.ucm.fdi.iw.model;
 
+import javax.persistence.*;
+
+@Entity
 public class Valoracion {
 
+	private long id;
+	private Usuario emisor;
+	private Propuesta propuesta;
+	private String valoracion;
+	private int puntuacion;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	public long getId() {
+		return id;
+	}
+	
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	@OneToOne(targetEntity = Usuario.class)
+	public Usuario getEmisor() {
+		return emisor;
+	}
+
+	public void setEmisor(Usuario emisor) {
+		this.emisor = emisor;
+	}
+
+	@OneToOne(targetEntity = Propuesta.class)
+	public Propuesta getPropuesta() {
+		return propuesta;
+	}
+
+	public void setPropuesta(Propuesta propuesta) {
+		this.propuesta = propuesta;
+	}
+
+	public String getValoracion() {
+		return valoracion;
+	}
+
+	public void setValoracion(String valoracion) {
+		this.valoracion = valoracion;
+	}
+
+	public int getPuntuacion() {
+		return puntuacion;
+	}
+
+	public void setPuntuacion(int puntuacion) {
+		this.puntuacion = puntuacion;
+	}
 }
