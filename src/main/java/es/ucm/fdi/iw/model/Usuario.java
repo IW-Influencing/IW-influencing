517b54aa97405ce1e675165d721be6cbe825fd69
Usuario Local <calopezd@ucm.es>
2020-03-10 15:40:51 +0100

    gran renombrado y limpieza

---
+++ src/main/java/es/ucm/fdi/iw/model/Usuario.java
@@ -0,0 +1,140 @@
+package es.ucm.fdi.iw.model;
+
+import java.util.Arrays;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Entity
+@NamedQueries({
+	@NamedQuery(name="Usuario.byNombreUsuario",
+	query="SELECT u FROM Usuario u "
+			+ "WHERE u.nombre = :nombre AND u.activo = 1"),
+	@NamedQuery(name="Usuario.hasNombre",
+	query="SELECT COUNT(u) "
+			+ "FROM Usuario u "
+			+ "WHERE u.nombre = :nombre")
+})
+public class Usuario {
+
+	private static BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+	
+	private long id; // Id unico para cada usuario
+	private byte activo;
+	private String nombre;
+	private String password;
+	public enum Rol {ADMIN, INFLUENCER, EMPRESA, USER};
+	private String roles; // Aqui se almacena el rol que tiene el usuario en la aplicación mediante números
+	private String apellidos;
+	private int edad;
+	private String tags; // Se almacenan los tags
+
+	
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+
+	public String getRoles() {
+		return roles;
+	}
+
+	public void setRoles(String roles) {
+		this.roles = roles;
+	}
+	
+	/**
+	 * Tests a raw (non-encoded) password against the stored one.
+	 * @param rawPassword to test against
+	 * @return true if encoding rawPassword with correct salt (from old password)
+	 * matches old password. That is, true iff the password is correct  
+	 */
+	public boolean passwordMatches(String rawPassword) {
+		return encoder.matches(rawPassword, this.password);
+	}
+
+	/**
+	 * Encodes a password, so that it can be saved for future checking. Notice
+	 * that encoding the same password multiple times will yield different
+	 * encodings, since encodings contain a randomly-generated salt.
+	 * @param rawPassword to encode
+	 * @return the encoded password (typically a 60-character string)
+	 * for example, a possible encoding of "test" is 
+	 * $2y$12$XCKz0zjXAP6hsFyVc8MucOzx6ER6IsC1qo5zQbclxhddR1t6SfrHm
+	 */
+	public static String encodePassword(String rawPassword) {
+		return encoder.encode(rawPassword);
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	public String getTags() {
+		return tags;
+	}
+
+	public void setTags(String tags) {
+		this.tags = tags;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String contraseña) {
+		this.password = contraseña;
+	}
+
+	public byte getActivo() {
+		return activo;
+	}
+
+	public void setActivo(byte activo) {
+		this.activo = activo;
+	}
+	
+	/**
+	 * Checks whether this user has a given role.
+	 * @param role to check
+	 * @return true iff this user has that role.
+	 */
+	public boolean hasRole(Rol role) {
+		String roleName = role.name();
+		return Arrays.stream(roles.split(","))
+				.anyMatch(r -> r.equals(roleName));
+	}
+	
+
+}
---
+++ src/main/resources/import.sql
@@ -0,0 +1,25 @@
+-- 
+-- El contenido de este fichero se cargará al arrancar la aplicación, suponiendo que uses
+-- 		application-default ó application-externaldb en modo 'create'
+--
+
+-- Usuario de ejemplo con nombre = a y contraseña = aa  
+INSERT INTO usuario(id,activo,nombre,password,roles,apellidos,edad,tags) VALUES (
+	1, 1, 'a', 
+	'{bcrypt}$2a$10$xLFtBIXGtYvAbRqM95JhcOaG23fHRpDoZIJrsF2cCff9xEHTTdK1u',
+	'ADMIN',
+	'Abundio Ejémplez',
+	23,
+	'ropa'
+	
+);
+
+-- Otro usuario de ejemplo con username = b y contraseña = aa  
+INSERT INTO usuario(id,activo,nombre,password,roles,apellidos,edad,tags) VALUES (
+	2, 1, 'b', 
+	'{bcrypt}$2a$10$xLFtBIXGtYvAbRqM95JhcOaG23fHRpDoZIJrsF2cCff9xEHTTdK1u',
+	'USER',
+	'Berta Muéstrez',
+	22,
+	'fiesta'
+);
