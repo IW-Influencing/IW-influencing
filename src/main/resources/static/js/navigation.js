76371bb0491c1d6d8f64fcfca207f2e12430b106
loptor5 <calopezd@ucm.es>
2020-05-04 00:18:10 +0200

    Reordenamiento, comentarios y arreglando cosas

--- src/main/resources/static/js/navigation.js
+++ src/main/resources/static/js/navigation.js
@@ -14,3 +14,18 @@ function cargaNotificacionesModalNav(){
 	return go2(config.rootUrl + "notificaciones", 'GET')
 		.then(html => document.getElementById("contenidoModal").innerHTML=html);
 }
+
+// Desplegable RRSS
+function abreEmpresa(evt, nombreEmpresa) {
+	let i, x, tablinks;
+	x = document.getElementsByClassName("empresa");
+	for (i = 0; i < x.length; i++) {
+		x[i].style.display = "none";
+	}
+	tablinks = document.getElementsByClassName("tablink");
+	for (i = 0; i < x.length; i++) {
+		tablinks[i].className = tablinks[i].className.replace(" w3-border-red", "");
+	}
+	document.getElementById(nombreEmpresa).style.display = "block";
+	evt.currentTarget.firstElementChild.className += " w3-border-red";
+}
